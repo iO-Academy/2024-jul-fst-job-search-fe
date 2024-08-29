1e76a230924dcb11de5ee8ad2aa52ae595ed2872
Joseph Padfield <joseph.padfield@gmail.com>
2024-08-29 10:27:56 +0100

    almost finished ocmponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import 'bootstrap'
 import './App.css'
 import JobSearch from "./components/JobSearch/index.jsx";
-import JobDescriptionModal from "./components/JobDescriptionModal/index.jsx";
-import JobListings from "./components/JobListings/index.jsx";
-import Nav from "./components/Navbar/index.jsx";
-import Footer from "./components/Footer/index.jsx";
-import {useEffect, useState} from "react";
 
 function App() {
     return (
 
         <>
             <JobSearch/>
-
-            {/*<Nav/>*/}
-            {/*<JobListings/>*/}
-            {/*<Footer/>*/}
-            {/*<JobDescriptionModal id={70}/>*/}
 
         </>
     )
--- src/components/JobSearch/index.jsx
+++ src/components/JobSearch/index.jsx
@@ -1,24 +1,35 @@
 import SkillsBadge from "../SkillsBadge/index.jsx";
-import button from "bootstrap/js/src/button.js";
 import './style.css'
-import {useState} from "react";
-// import {useEffect, useState} from "react";
+import {useState, useEffect} from "react";
 const JobSearch = () => {
 
-    const handleClick = (event) => {
-        console.log(event.target)
+    const [input, setInput] = useState('')
+    const [query, setQuery] = useState('')
+    const [queryReturn, setQueryReturn] = useState([])
+
+    const takeInput = (event) => {
+        setInput(event.target.value)
     }
 
-    let query = ''
+    const constructQuery = () => {
+        setQuery('http://0.0.0.0:8080/jobs?search='+input)
+    }
 
+    useEffect (() => {
+        const getData = async () => {
+            let response = await fetch(query)
+            let data = await response.json()
+            setQueryReturn(data)
+        }
+        getData()
+    },[query])
 
 
     return (
         <div className={'backgroundPic container-fluid bg-secondary py-5 px-4'}>
             <div className={'container-fluid col-lg-7'}>
                 <h1 className={'text-white display-1 py-3'}>Find your perfect job</h1>
-                <form>
-                <input className={'col-12 rounded-2 border-0'} onChange={handleClick} type={'text'} placeholder={'Keyword/Skill/Company Name'} name={'searchTerm'} id={'searchTerm'}/>
+                <input className={'col-12 rounded-2 border-0'} onChange={takeInput} type={'text'} placeholder={'Keyword/Skill/Company Name'} name={'searchTerm'} id={'searchTerm'}/>
                 <div className={'d-flex flex-row py-2'}>
                     <div className={'checkboxes container-fluid d-flex flex-wrap gap-1 p-1'}>
                         <div className={'form-check'}>
@@ -43,9 +54,9 @@ const JobSearch = () => {
                         </div>
                     </div>
                 </div>
-                <button className={'btn btn-primary fs-4 py-2'} type={'submit'}>Search</button>
-                </form>
+                <button className={'btn btn-primary fs-4 py-2'} onClick={constructQuery}>Search</button>
             </div>
+        {/*    TABLE GOES HERE*/}
         </div>
     )
 }
