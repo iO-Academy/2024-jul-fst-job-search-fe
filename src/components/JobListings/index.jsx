9bfae0413947b4105a0929e3e358b94f8d087777
Ramsey Merdassi <merdassi.r@gmail.com>
2024-08-29 16:50:29 +0100

    added search result functionality

--- src/components/JobListings/index.jsx
+++ src/components/JobListings/index.jsx
@@ -2,17 +2,17 @@ import Header from "../Header/index.jsx";
 import Table from "../Table/index.jsx";
 import {useEffect, useState} from "react";
 
-const JobListings = () => {
-    const [headerText, setHeaderText] = useState('Most recent jobs')
-    const [jobInfo, setJobInfo] = useState([])
+const JobListings = ({headerText, jobInfo, setJobInfo, url}) => {
+    // const [headerText, setHeaderText] = useState('Most recent jobs')
+    // const [jobInfo, setJobInfo] = useState([])
     useEffect(() => {
-        const getJobInfo = async () => {
-            const response = await fetch('http://0.0.0.0:8080/jobs/recent')
+        const getJobInfo = async (url) => {
+            const response = await fetch(url)
             const data = await response.json()
             setJobInfo(data)
         }
-        getJobInfo()
-    }, []);
+        getJobInfo(url)
+    }, [url]);
     return (
         <main className='mx-auto container justify-content-center col-12 col-md-7 p-0'>
                 <Header headerText={headerText} />
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,11 +1,10 @@
 import {useEffect, useState} from "react";
 
-const Skills = () => {
+const Skills = ({setHeaderText, setUrl}) => {
 
 const [skills, setSkills] = useState([])
 
     useEffect( () => {
-
         const getSkills = async () => {
             const response = await fetch('http://0.0.0.0:8080/skills')
             const data = await response.json()
@@ -15,18 +14,21 @@ const [skills, setSkills] = useState([])
         },[]
     )
 
-    console.log(skills)
+    const clickSkill = (id) => {
+        setHeaderText('Search results')
+        setUrl('http://0.0.0.0:8080/jobs?skill='+id)
+    }
 
     return (
         <div className='text-bg-primary py-3 d-flex flex-column px-4 col-md-12'>
                 <div className='d-flex justify-content-center mx-2 col-md-6'>
-                    <h2 className='text-black px-0' >Browse skills</h2>
+                    <h2 className='text-black px-0'>Browse skills</h2>
                 </div>
                 <div className='container d-flex flex-wrap justify-content-center col-md-8 p-7'>
                     {
-                        skills.map((item) => {
+                        skills.map((item, index) => {
                             return (
-                                <div className='border border-black pb-2 col-5 col-md-2 m-2 m-md-3 p-7'>
+                                    <div key={index} onClick={() => clickSkill(item.id)} className='border border-black pb-2 col-5 col-md-2 m-2 m-md-3 p-7'>
                                     <h2 className='ms-1 pt-2 text-center p-7'>{item.skill}</h2>
                                     <h6 className='text-dark ms-1 text-center'>{item.job_count} Jobs</h6>
                                 </div>
